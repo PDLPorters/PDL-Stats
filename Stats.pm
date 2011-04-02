36b8c79d205dc6177b6004668f59cb1219f5da7d
Maggie J Xiong <maggiexyz@users.sourceforge.net>
2011-04-02 13:58:08 -0400

    Upgraded dependency to PDL 2.4.8. Updated version.

--- Stats.pm
+++ Stats.pm
@@ -8,11 +8,11 @@ PDL::Stats - a collection of statistics modules in Perl Data Language, with a qu
 
 =head1 VERSION
 
-Version 0.4.4
+Version 0.5.0
 
 =cut
 
-$VERSION = '0.4.4';
+$VERSION = '0.5.0';
 
 $PDL::onlinedoc->scan(__FILE__) if $PDL::onlinedoc;
 
