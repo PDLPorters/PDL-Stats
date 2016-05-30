19d0dfe51da1ceb882a82df461c3a49884c32461
Ed J <mohawk2@users.noreply.github.com>
2016-05-30 20:40:39 +0100

    0.75

--- Stats.pm
+++ Stats.pm
@@ -6,7 +6,7 @@ PDL::Stats - a collection of statistics modules in Perl Data Language, with a qu
 
 =cut
 
-$VERSION = '0.74_1';
+$VERSION = '0.75';
 
 $PDL::onlinedoc->scan(__FILE__) if $PDL::onlinedoc;
 
